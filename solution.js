15d753294f2ab4e83011dae3c3421fcba468a54a
Iliana Chakarova <chaka@Iliana_DELL>
2024-05-25 16:50:09 +0200

    Implemented Search form functionality

--- solution.js
+++ solution.js
@@ -16,6 +16,25 @@ function changeContent(className) {
     }
 }
 
+changeContent('search-form-content');
+document.querySelector('#search-form-button').addEventListener('click', (e) => searchFormData(e));
+
+function searchFormData(e) {
+    e.preventDefault();
+    const data = e.target.parentElement;
+    const checkIn = data.querySelector('#check-in').value;
+    const checkOut = data.querySelector('#check-out').value;
+    const people = data.querySelector('#people').value;
+    if (checkIn != '' && checkOut != '' && people != '' &&
+        new Date(checkIn) <= new Date(checkOut)) {
+        reservation.startDate = checkIn;
+        reservation.endDate = checkOut;
+        reservation.guestsCount = people;
+        console.log(reservation);
+        changeContent('search-result-form-content');
+    }
+}
+
 document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
 
 function cleanData(e) {
